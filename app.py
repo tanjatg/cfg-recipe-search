06582be899b293ab741eb8b4a5df001a968ff741
Tanja <tanja@Tanjas-MBP.ldabiralai.lan>
2023-11-21 20:27:14 +0000

    Adjusted the homepage_search function and added some stuff on html

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
-import edamam_api
+from edamam_api import recipe_request
 
 app = Flask(__name__)
-
-
 @app.get("/")
-def recipe_search():
-
+def homepage_search():
+    search_request = recipe_request()
+    return render_template("homepage.html", search_request=search_request)
 
 app.run(debug=True)
