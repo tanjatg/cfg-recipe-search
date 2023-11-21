3bf67aaf2966f2f253e6bc3d7378856832c84338
Tanja <tanja@Tanjas-MBP.ldabiralai.lan>
2023-11-21 23:26:58 +0000

    Added search and random feature to homepage

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template, request
-from edamam_api import recipe_request
+from flask import Flask, render_template, request, session
+from edamam_api import random_recipe
 
 app = Flask(__name__)
-@app.get("/")
-def homepage_search():
-    search_request = recipe_request()
-    return render_template("homepage.html", search_request=search_request)
 
-@app.get("/<name>")
-def display_recipes():
+# app.secret_key = secrets.token_hex()
+# print(app.secret_key)
 
+
+@app.route('/', methods=['POST'])
+def homepage_form_post():
+    ingredient = request.form['ingredient']
+    return render_template('homepage.html', ingredient=ingredient)
+
+@app.get('/')
+def homepage_random_ingredient():
+    rand_recipe = random_recipe()
+    rand_ingredient = rand_recipe[0]
+    return render_template('homepage.html', rand_ingredient=rand_ingredient, rand_recipe=rand_recipe)
+
+# @app.get("/<name>")
+# def display_recipes():
+#
 
 app.run(debug=True)
--- edamam_api.py
+++ edamam_api.py
@@ -1,4 +1,5 @@
 import requests
+import random
 
 # querystring = {"type" :"public","co2EmissionsClass":"A+","field[0]":"uri","beta":"true","random":"true","cuisineType[0]":"American","imageSize[0]":"LARGE","mealType[0]":"Breakfast","health[0]":"alcohol-cocktail","diet[0]":"balanced","dishType[0]":"Biscuits and cookies"}
 #
@@ -12,15 +13,18 @@ import requests
 # recipe_test = REQUEST.json()
 #
 # print(recipe_test)
+
+
 def recipe_search(ingredient):
     APP_ID = "1615c37c"
     APP_KEY = "55c93edaaee28bc3ad1ffea9ca66f8b4"
     result = requests.get(
-    'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, APP_ID,
-    APP_KEY)
+    'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, APP_ID, APP_KEY)
     )
     data = result.json()
     return data['hits']
+
+
 def run():
     ingredient = input('What do you want to search for? ')
     results = recipe_search(ingredient)
@@ -31,4 +35,38 @@ def run():
         print(recipe['image'])
         print()
 
-run()
+def random_ingredient():
+    ingredients = [
+        "almond", "anchovy", "apple", "apricot", "artichoke", "asparagus", "avocado", "aubergine",
+        "bacon", "beans", "banana", "bamboo", "barley", "basil", "beef", "beer", "beet", "berry", "bream", "bok choi", "brie", "bulgur", "butternut", "buttermilk", "broccoli", "bell pepper",
+        "cabbage", "cake", "camembert", "capers", "carrot", "cashew", "cauliflower", "catfish", "cavolo nero", "celery", "cheese", "cherry", "chestnut", "chicken", "chickpea", "chocolate", "chorizo", "coconut", "coffee", "coriander", "corn", "crab", "couscous", "courgette", "cress", "cucumber", "currant", "cumin",
+        "daikon", "dandelion", "date", "dill", "duck",
+        "eel", "egg", "elderflower",
+        "fennel", "fig", "feta", "flour", "fish", "fruit",
+        "gammon", "garlic", "ginger", "gnocchi", "grape", "goose",
+        "haddock", "halloumi", "hare", "ham", "hazelnut", "honey", "horseradish",
+        "jam", "kale", "ketchup", "kiwi", "kohlrabi", "kipper",
+        "lamb", "lardons", "leek", "lemon", "lime", "lettuce", "lentil",
+        "mackerel", "mango", "meat", "mayonnaise", "melon", "milk", "mint", "mince", "miso", "mushroom", "mustard", "mutton",
+        "noodles", "nuts", "nutmeg", "nettle",
+        "oats", "octopus", "olive", "okra", "onion", "orange", "oregano", "oyster",
+        "pak choi", "pancetta", "paneer", "papaya", "pasta", "parmesan", "paprika", "passion fruit", "passata", "parsnip", "parsley", "pastry", "peach", "peas", "peanut", "pear", "pecan", "pheasant", "pineapple", "pistachio", "plaice", "plantain", "pomegranate", "plum", "pork", "potato", "prawn", "pumpkin",
+        "quail", "quinoa", "rabbit", "radish", "raisins", "rice", "rhubarb", "risotto", "rosemary", "rocket", "ricotta",
+        "sage", "salad", "salmon", "sardine", "sausage", "scallop", "sea bass", "seafood", "seaweed", "sesame", "shallot", "soup", "spaghetti", "squid", "spring onion", "spinach", "steak", "swede", "sweet potato",
+        "tea", "teriyaki", "tofu", "tilapia", "tomato", "turnip", "turmeric", "truffle", "tortellini", "trout", "turkey",
+        "vanilla", "veal", "venison", "walnut", "wine", "yam", "yoghurt"
+    ]
+    random_ingredient = random.choice(ingredients)
+    return random_ingredient
+
+def random_recipe():
+    rand_ingredient = random_ingredient()
+    ingredient = rand_ingredient
+    list = [ingredient]
+    results = recipe_search(ingredient)
+    for result in results:
+        recipe = result['recipe']['label']
+        url = result['recipe']['url']
+        list.append({'recipe': recipe, 'url': url})
+    return list
+random_recipe()
