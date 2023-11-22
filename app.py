eb91b88a6152d1cdb592cbde1c669bdd5e4ce8d3
Tanja <tanja@Tanjas-MBP.ldabiralai.lan>
2023-11-22 19:02:54 +0000

    Changed the HTML form to get and adjusted app.py route so pages connect

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request
 from edamam_api import random_recipe, recipe_search
 
 app = Flask(__name__)
 
-# app.secret_key = secrets.token_hex()
-# print(app.secret_key)
-
-
-@app.route('/', methods=['POST'])
-def homepage_form_post():
-        ingredient = request.form['ingredient']
-        return render_template('homepage.html', ingredient=ingredient)
 
 @app.get('/')
 def homepage_random_ingredient():
@@ -18,9 +10,12 @@ def homepage_random_ingredient():
     rand_ingredient = rand_recipe[0]
     return render_template('homepage.html', rand_ingredient=rand_ingredient, rand_recipe=rand_recipe)
 
-@app.get("/<ingredient>")
-def display_recipes(ingredient):
+
+@app.get("/recipes")
+def display_recipes():
+    ingredient = request.args.get('ingredient')
     recipe_list = recipe_search(ingredient)
     return render_template('search_results.html', ingredient=ingredient, recipe_list=recipe_list)
+
 
 app.run(debug=True)
